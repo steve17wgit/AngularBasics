39635c0a748d20c7ca9c5990f3c1a1938e8a1aa6
steve17wgit <steve.woods.work.ph@gmail.com>
2018-03-19 15:54:43 +0800

    angular basics -initial creation

---
+++ AngularBasics/Controllers/AngularController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularBasics.Controllers
+{
+    public class AngularController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Intro()
+        {
+            return View();
+        }
+
+        public IActionResult Expressions()
+        {
+            return View();
+        }
+
+        public IActionResult Modules()
+        {
+            return View();
+        }
+
+        public IActionResult Directives()
+        {
+            return View();
+        }
+
+        public IActionResult Model()
+        {
+            return View();
+        }
+
+        public IActionResult DataBinding()
+        {
+            return View();
+        }
+
+        public IActionResult Controllers()
+        {
+            return View();
+        }
+
+        public IActionResult Scope()
+        {
+            return View();
+        }
+
+        public IActionResult Filters()
+        {
+            return View();
+        }
+    }
+}
---
+++ AngularBasics/wwwroot/js/namesController.js
@@ -0,0 +1,17 @@
+﻿angular.module('myApp', []).controller('namesCtrl', function ($scope) {
+    $scope.names = [
+        { name: 'Jani', country: 'Norway' },
+        { name: 'Carl', country: 'Sweden' },
+        { name: 'Margareth', country: 'England' },
+        { name: 'Hege', country: 'Norway' },
+        { name: 'Joe', country: 'Denmark' },
+        { name: 'Gustav', country: 'Sweden' },
+        { name: 'Birgit', country: 'Denmark' },
+        { name: 'Mary', country: 'England' },
+        { name: 'Kai', country: 'Norway' }
+    ];
+    $scope.orderByMe = function (x) {
+        $scope.myOrderBy = x;
+    }
+
+});
